a6c732bafa7677fd0f202805d8fe632477cfb2bb
Mohammad Salahadeen <salahadeenm@gmail.com>
2026-09-24 20:34:48 +0200

    sheet naming

--- netlify/functions/sheet.js
+++ netlify/functions/sheet.js
@@ -1,8 +1,8 @@
 // Allowed sheets (key -> Google Sheet ID)
 const SHEETS = {
   bbb: '1UInMmMBWA5zvcy4vwSf2XwtxpBqXASiNDkZ-S6Bv_Cg',
-  aaa: '1U2ZmiGJfotFVYGRGytANJosiWQJHJcMSSoMzP4DUU4Q',
-  HiddenPotential: '1pRSSMA6oWZDkuIwaXmPXA9CivUhCw5jqL68MLPTQYWU'
+  FootballSessions: '1U2ZmiGJfotFVYGRGytANJosiWQJHJcMSSoMzP4DUU4Q',
+  HiddenPotential: '1U2ZmiGJfotFVYGRGytANJosiWQJHJcMSSoMzP4DUU4Q'
 };
 // Proper CSV parser: handles "quoted, cells", "" escaped quotes and line breaks inside cells
 function parseCSV(text) {
